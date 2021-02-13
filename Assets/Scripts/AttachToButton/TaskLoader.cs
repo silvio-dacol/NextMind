a40c3c4e2001c83a86eca1f3972db617ce5d2672
Sisa12 <dacol@uwindsor.ca>
2021-02-13 13:11:01 -0500

    Scene Task Switch
    Now the buttons can switch between one task and another

---
+++ Assets/Scripts/AttachToButton/TaskLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+//Script to be attached to the button: be sure the name of button and name of the scene coincide
+
+public class TaskLoader : MonoBehaviour
+{
+    public bool startOverallSceneTime;
+    public bool startTaskExercise;
+
+    public void LoadTask()
+    {
+        //I get and store all the scenes in an array
+        int countLoaded = SceneManager.sceneCount;
+        Scene[] loadedScenes = new Scene[countLoaded];
+
+        for(int i = 0; i < countLoaded; i++)
+        {
+            loadedScenes[i] = SceneManager.GetSceneAt(i);
+        }
+
+        //I unload the loaded scenes except the MainMenu one (which contains the essential things)
+        for(int i = 0; i < countLoaded; i++)
+        {
+            if(loadedScenes[i].name != "MainMenu")
+            {
+                SceneManager.UnloadSceneAsync(loadedScenes[i].name);
+            }
+        }
+
+        //I load the scene relative to that specific task button
+        SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+
+        //For the future, see how to implement the passing of time
+        startOverallSceneTime = true;
+        startTaskExercise = true;
+    }
+}
