c3422420e9713161c27ea7d098c0b96aebbc7b3a
Sisa12 <dacol@uwindsor.ca>
2021-02-14 00:34:37 -0500

    Button Toggle Solved

--- Assets/Scripts/AttachToButton/ToggleTrigger.cs
+++ Assets/Scripts/AttachToButton/ToggleTrigger.cs
@@ -3,22 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Microsoft.MixedReality.Toolkit.UI;
+using NextMind.NeuroTags;
 
 //Script to be attached to NextMind and HoloLens buttons to activate or deactivate the respective objects components
 
 public class ToggleTrigger : MonoBehaviour
 {
-    //The ObjectContainer object in each of the task scenes
-    //private GameObject objectContainer;
+    private Transform objectContainer;
 
-    //public void HoloLensTrigger()
-    //{
-    //    bool isHoloLensActive = true;
-    //}
+    public void HoloLensTriggerOn()
+    {
+        objectContainer = GameObject.Find("ObjectContainer").GetComponent<Transform>();
 
-    //public void NextMindTrigger()
-    //{
-    //    bool isNextMindActive = true;
-    //}
+        Transform child;
+
+        for(int i = 0; i < objectContainer.childCount; i++)
+        {
+            child = objectContainer.GetChild(i);
+            child.GetComponent<Interactable>().enabled = true;
+        }
+    }
+
+    public void HoloLensTriggerOff()
+    {
+        objectContainer = GameObject.Find("ObjectContainer").GetComponent<Transform>();
+
+        Transform child;
+
+        for (int i = 0; i < objectContainer.childCount; i++)
+        {
+            child = objectContainer.GetChild(i);
+            child.GetComponent<Interactable>().enabled = false;
+        }
+    }
+
+    public void NextMindTriggerOn()
+    {
+        objectContainer = GameObject.Find("ObjectContainer").GetComponent<Transform>();
+
+        Transform child;
+
+        for (int i = 0; i < objectContainer.childCount; i++)
+        {
+            child = objectContainer.GetChild(i);
+            child.GetComponent<NeuroTag>().enabled = true;
+        }
+    }
+
+    public void NextMindTriggerOff()
+    {
+        objectContainer = GameObject.Find("ObjectContainer").GetComponent<Transform>();
+
+        Transform child;
+
+        for (int i = 0; i < objectContainer.childCount; i++)
+        {
+            child = objectContainer.GetChild(i);
+            child.GetComponent<NeuroTag>().enabled = false;
+        }
+    }
 }
 
