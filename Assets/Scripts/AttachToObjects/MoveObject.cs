5bf2afcc0c0156b1b4d5979966577bda81688d0f
Sisa12 <dacol@uwindsor.ca>
2021-03-03 13:12:05 -0500

    Object Movement added in Task3 scene

--- Assets/Scripts/AttachToObjects/MoveObject.cs
+++ Assets/Scripts/AttachToObjects/MoveObject.cs
@@ -6,52 +6,72 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
-    float speed;
-    float speedMultiplier;
-    int randomNumber;
+    //How much I want this thing to move
+    private Vector3 deltaPosition;
 
-    void Start()
+    [Tooltip("The direction along which the object moves")]
+    [SerializeField] private Vector3 normalisedPositionVector;
+
+    [Tooltip("Speed to go from point A to point B")]
+    [SerializeField] private float speed = 0.2f;
+
+    [Tooltip("The range (+/-) used to access the vector direction")]
+    [SerializeField] private float range = 1f;
+
+    //The index allowing to change direction back and forth
+    private int changeDirectionIndex;
+
+    //How many frames wait before changing direction
+    [SerializeField] private int deltaFrames;
+
+    //The frame when I start to count
+    private int initialFrames;
+
+    private void Start()
     {
-        //I get a random game object between the child of the parent ObjectContainer
-        randomNumber = Random.Range(0, 2);
+        //Initialisation in Start() just to be sure
+        speed = 0.2f;
+        range = 1.5f;
 
-        //I set the speedMultiplier
-        speedMultiplier = 3;
+        //It's a random direction for the movement of that specific object
+        deltaPosition = new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
 
-        //I set the speed of movement
-        speed = randomNumber * speedMultiplier;
+        //I define the deltaFrames to change direction just between 700 and 900
+        deltaFrames = Random.Range(700,900);
+
+        //This will be the actual direction of my movement
+        normalisedPositionVector = deltaPosition.normalized;
+
+        //I initialise the initialFrames at zero
+        initialFrames = Time.frameCount;
     }
 
-    void Update()
+    private void Update()
     {
-        if (randomNumber == 0)
+        if(changeDirectionIndex == 0)
         {
-            Movement0();
+            gameObject.transform.position = gameObject.transform.position + (normalisedPositionVector * speed * Time.deltaTime);
+
+            if(Time.frameCount - initialFrames >= deltaFrames)
+            {
+                //I initialise again the initialFrames for the next position changing
+                initialFrames = Time.frameCount;
+
+                //I change the changeDirectionIndex, so the script can enter the other if statement
+                changeDirectionIndex = 1;
+            }
         }
 
-        if (randomNumber == 1)
+        else if(changeDirectionIndex == 1)
         {
-            Movement1();
+            gameObject.transform.position = gameObject.transform.position + (-normalisedPositionVector * speed * Time.deltaTime);
+
+            if (Time.frameCount - initialFrames >= deltaFrames)
+            {
+                initialFrames = Time.frameCount;
+
+                changeDirectionIndex = 0;
+            }
         }
-
-        if (randomNumber == 2)
-        {
-            Movement2();
-        }
-    }
-
-    private void Movement0()
-    {
-        
-    }
-
-    private void Movement1()
-    {
-        
-    }
-
-    private void Movement2()
-    {
-
     }
 }
